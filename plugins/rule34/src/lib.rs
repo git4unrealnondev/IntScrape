2a930a59c001ebe5f12dfa4e11b73344d58754ef
git4unrealnondev <30579641+git4unrealnondev@users.noreply.github.com>
2026-08-25 22:19:15 -0500

    Adds job limiting for rule34

--- plugins/rule34/src/lib.rs
+++ plugins/rule34/src/lib.rs
@@ -38,6 +38,7 @@ fn get_plugin_info() -> Vec<shared_types::Plugin> {
     vec![shared_types::Plugin {
         name: "Rule34".into(),
         properties: vec![
+            PluginProperties::JobNum(10),
             PluginProperties::Ratelimit(1, std::time::Duration::from_secs(1)),
             PluginProperties::Sites(vec!["r34".into(), "rule34".into(), "rule34.xxx".into()]),
             PluginProperties::Login((
